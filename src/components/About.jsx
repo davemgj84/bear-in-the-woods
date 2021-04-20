91d8d8bb80ea8a5af49bbe03a1605756bdf41621
David <davemgj@gmail.com>
2021-04-20 13:53:41 -0700

    add react responsive image carousel package - start exploring it in about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,45 @@
 import "../styles/About.scss";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import concreteOne from "../assets/concrete/concrete1.jpg";
+import concreteTwo from "../assets/concrete/concrete2.jpg";
+import concreteThree from "../assets/concrete/concrete3.jpg";
+import concreteFour from "../assets/concrete/concrete4.jpg";
+import concreteFive from "../assets/concrete/concrete5.jpg";
+import concreteSix from "../assets/concrete/concrete6.jpg";
 
 const About = () => {
-  return <section className="about"></section>;
+  return (
+    <section className="about">
+      <Carousel
+        className="gallery"
+        width={"700px"}
+        swipeable={true}
+        infiniteLoop={true}
+        useKeyboardArrows={true}
+        showStatus={false}
+      >
+        <div>
+          <img src={concreteOne} alt="Concrete Work" />
+        </div>
+        <div>
+          <img src={concreteTwo} alt="Concrete Work" />
+        </div>
+        <div>
+          <img src={concreteThree} alt="Concrete Work" />
+        </div>
+        <div>
+          <img src={concreteFour} alt="Concrete Work" />
+        </div>
+        <div>
+          <img src={concreteFive} alt="Concrete Work" />
+        </div>
+        <div>
+          <img src={concreteSix} alt="Concrete Work" />
+        </div>
+      </Carousel>
+    </section>
+  );
 };
 
 export default About;
